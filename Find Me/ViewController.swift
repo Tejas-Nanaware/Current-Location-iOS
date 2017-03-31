48ca7bb702ee34157e6a0a4f1c2e87ee21894f75
admin <admin@ACE20.local>
2017-03-31 11:58:43 +0530

    Integrated map

--- Find Me/ViewController.swift
+++ Find Me/ViewController.swift
@@ -24,9 +24,29 @@ class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDele
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation = locations[0]
+        let lat = userLocation.coordinate.latitude
+        let lon = userLocation.coordinate.longitude
+        let latDelta : CLLocationDegrees = 0.005
+        let lonDelta : CLLocationDegrees = 0.005
+        let span = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+        let region = MKCoordinateRegion(center: location, span: span)
+        map.setRegion(region, animated: true)
+        latitude.text = String(lat)
+        longitude.text = String(lon)
+        altitude.text = String(userLocation.altitude)
+        speed.text = String(userLocation.speed) + " mph"
+        course.text = String(userLocation.course)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
